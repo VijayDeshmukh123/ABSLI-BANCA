9b7ccdfb700492731e841b027d9a2e8954c6a8c0
VijayDeshmukh123 <vijaydeshmukh647@gmail.com>
2024-02-10 14:13:58 +0530

    some d

--- src/test/java/com/absli/testcases/TestRunner.java
+++ src/test/java/com/absli/testcases/TestRunner.java
@@ -1,40 +1,12 @@
 package com.absli.testcases;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 
-import Utils.ExcelUtils;
-import com.absli.base.BasePage;
-import com.absli.scripts.Inilazation;
-import com.absli.utils.PropertiesUtils;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import com.absli.TestFactory.GotoEndtoEnd.EndToEndProMajorJourney;
-import com.absli.TestFactory.GotoEndtoEnd.GoToPMSpage_Major_Minor_Journey;
 import com.absli.base.TestBase;
 import com.absli.dataproviders.DataProviders;
-import com.absli.scripts.EndtoEndProposerDiff_Major;
-import com.absli.scripts.EndtoEndProposerDiff_Minor;
 import com.absli.scripts.EndtoEndProposerSame;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
+
 //import static com.absli.base.TestBase.prop;
 public class TestRunner extends TestBase{
 
