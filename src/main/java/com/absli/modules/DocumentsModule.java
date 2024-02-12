a278446dac68fb0cfa617692f1452b20cffad800
VijayDeshmukh123 <vijaydeshmukh647@gmail.com>
2024-02-12 21:21:12 +0530

    pom

--- src/main/java/com/absli/modules/DocumentsModule.java
+++ src/main/java/com/absli/modules/DocumentsModule.java
@@ -76,213 +76,52 @@ public class DocumentsModule {
 //------------------------------------------------------Same ------------------------------------------------------------------//
 
 	public void Documentsmodule(WebDriver driver, HashMap<String, String> testData) throws InterruptedException, IOException, ParseException {
-	// Document Upload //
-			List<WebElement> documentpageCount = getDriver().findElements(By.xpath("(//div[@role='tablist'])[2]//button//span[1]"));
-			int docSize = documentpageCount.size();
-			for (int i = 0; i < docSize; i++) {
-				String str = documentpageCount.get(i).getText();
+		dashboard = new Dashboard(getDriver());
+		loginPage = new LoginPage(getDriver());
+		salesdetails = new SalesDetails(getDriver());
+		customerprofile = new CustomerProfile(getDriver());
+		viewquote = new ViewQuote(getDriver());
+		communicationdetails = new CommunicationDetails(getDriver());
+		bankdetails = new BankDetails(getDriver());
+		nominee = new Nominee(getDriver());
+		address = new AddressDetalis(getDriver());
+		proposerpersonalinfo = new ProposerPersonalInfo(getDriver());
+		proposerprofdetails = new ProposerProfessionalDetails(getDriver());
+		insurerpersonalinfo = new InsurerPersonalInfo(getDriver());
+		insuprofdetails = new InsuProfessionalDetails(getDriver());
+		insupersonalhealth = new InsurerPersonalHealth((getDriver()));
+		insurerlifestyle = new InsurerLifestyle(getDriver());
+		insurerfamilymedical = new InsurerFamilyMedical((getDriver()));
+		insurercoviddetails = new InsurerCovidDetails(getDriver());
+		insurermedicalhistory = new InsurerMedicalHistory(getDriver());
+		nriMandate = new ProposerNRIMandate(getDriver());
+		insurerNriMandate = new InsurerNRIMandate(getDriver());
+		insurerNriMandate = new InsurerNRIMandate(getDriver());
+		rna = new RandAPage(getDriver());
+		eccd = new ECCDPage(getDriver());
+		paymentpage = new PaymentPage(getDriver());
+		documentpage = new DocumentsPage(getDriver());
+		prop = new PropertiesUtils();
+		softassert = new SoftAssert();
+		healthandpuretermspage = new HealthAndPureTermsPage(getDriver());
+		IARpage=new IARPage(getDriver());
+		// Document Upload //
+		List<WebElement> documentpageCount = getDriver().findElements(By.xpath("(//div[@role='tablist'])[2]//button//span[1]"));
+		int docSize = documentpageCount.size();
+		for (int i = 0; i < docSize; i++) {
+			String str = documentpageCount.get(i).getText();
 
-				if (str.equalsIgnoreCase("Identity Proof")) {
-					if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO") || testData.get("PanNameEdit").equalsIgnoreCase("YES")) {
-						if (testData.get("IsPanAvailable").equalsIgnoreCase("NO") || testData.get("PanNameEdit").equalsIgnoreCase("YES") || testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
-							documentpage.clickOnIdentityTileweb();
-							wait.WaitTime2();
-							documentpage.SelectTypeOfDocumentweb(testData.get("SelectTypeOfDocument"));
-							documentpage.UploadFileweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-							TestUtil.scrollTillEndOfPage(getDriver());
-							Thread.sleep(2000);
-							documentpage.clickOnNEXTButtonweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-						} else {
-							TestUtil.scrollTillEndOfPage(getDriver());
-							wait.WaitTime2();
-							documentpage.clickOnNEXTButtonweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-						}
-					} else {
-						TestUtil.scrollTillEndOfPage(getDriver());
+			if (str.equalsIgnoreCase("Identity Proof")) {
+				if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO") || testData.get("PanNameEdit").equalsIgnoreCase("YES")) {
+					if (testData.get("IsPanAvailable").equalsIgnoreCase("NO") || testData.get("PanNameEdit").equalsIgnoreCase("YES") || testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
+						//	documentpage.clickOnIdentityTileweb();
 						wait.WaitTime2();
-						documentpage.clickOnNEXTButtonweb();
-						dashboard.checkLoaderWeb();
-						wait.WaitTime2();
-
-					}
-				}
-				if (str.equalsIgnoreCase("Address Proof")) {
-					if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO")
-							|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
-							|| testData.get("EditAddress").equalsIgnoreCase("YES")) {
-						if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")
-								|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
-								|| testData.get("EditAddress").equalsIgnoreCase("YES")
-								|| testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
-							documentpage.clickOnAddressProofTileweb();
-							wait.WaitTime2();
-							documentpage.SelectTypeOfDocumentweb(testData.get("SelectTypeOfDocument"));
-							documentpage.UploadFileweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-
-							if (testData.get("IsPanAvailable").equalsIgnoreCase("YES")
-									&& testData.get("IsNRI").equalsIgnoreCase("NO")
-									&& testData.get("EditAddress").equalsIgnoreCase("YES")
-									|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")
-											&& testData.get("IsNRI").equalsIgnoreCase("NO")
-											&& testData.get("IsPermanentAddressSame").equalsIgnoreCase("NO")
-									|| testData.get("IsPanAvailable").equalsIgnoreCase("YES")
-											&& testData.get("IsNRI").equalsIgnoreCase("YES")
-											&& testData.get("EditAddress").equalsIgnoreCase("YES")
-									|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")
-											&& testData.get("IsNRI").equalsIgnoreCase("YES")
-											&& testData.get("IsPermanentAddressSame").equalsIgnoreCase("NO")) {
-								documentpage.SelectTypeOfDocumentPermant(testData.get("SelectTypeOfDocument"));
-								documentpage.UploadFilepermant();
-								dashboard.checkLoaderWeb();
-							}
-							documentpage.clickOnNEXTButtonweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-						} else {
-							TestUtil.scrollTillEndOfPage(getDriver());
-							wait.WaitTime2();
-							documentpage.clickOnNEXTButtonweb();
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-						}
-					} else {
-						TestUtil.scrollTillEndOfPage(getDriver());
-						wait.WaitTime2();
-						documentpage.clickOnNEXTButtonweb();
-						dashboard.checkLoaderWeb();
-						wait.WaitTime2();
-
-					}
-
-				}
-				if (str.equalsIgnoreCase("Non Medical Requirement")) {
-					try {
-						documentpage.UploadNonMedicalNEFTCancelChequeProposerNotInsuredweb();
-						dashboard.checkLoaderWeb();
-					} catch (Exception e) {
-
-					}
-					if (testData.get("IsNRI").equalsIgnoreCase("NO")) {
-
-						if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")) {
-							documentpage.UploadNonMedicalForm60ProposerNotInsuredweb();
-
-							dashboard.checkLoaderWeb();
-							wait.WaitTime2();
-						}
-					}
-					try {
-						documentpage.UploadNonMedicalPhotograph();
-						dashboard.checkLoaderWeb();
-					} catch (Exception e) {
-
-					}
-
-					try {
-						documentpage.UploadNonMedicalPEPQuestionnaireweb();
-						dashboard.checkLoaderWeb();
-					} catch (Exception e) {
-
-					}
-					try {
-						documentpage.BANKSTATEMENTweb();
-						dashboard.checkLoaderWeb();
-					} catch (Exception e) {
-
-					}
-					try {
-						documentpage.FINANCIALTRANSACTIONFORMweb();
-						dashboard.checkLoaderWeb();
-					} catch (Exception e) {
-
-					}
-					if (testData.get("IsECDF").equalsIgnoreCase("NO")) {
-						documentpage.UploadNonMedicalCustomerDeclarationFormProposerNotInsuredweb();
-						dashboard.checkLoaderWeb();
-						Thread.sleep(2000);
-
-					}
-
-					TestUtil.scrollTillEndOfPage(getDriver());
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-					documentpage.clickOnNEXTButtonweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-
-				}
-				if (str.equalsIgnoreCase("Medical Requirement")) {
-					wait.WaitTime2();
-					TestUtil.scrollTillEndOfPage(getDriver());
-					wait.WaitTime2();
-					documentpage.clickOnNEXTButtonweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-				}
-				if (str.equalsIgnoreCase("Income Proof")) {
-					documentpage.clickOnIdentityTileweb();
-					wait.WaitTime2();
-					documentpage.SelectTypeOfDocumentIncomePrrofweb(testData.get("SelectTypeOfDocIncomeProof"));
-					documentpage.UploadFileweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-					TestUtil.scrollTillEndOfPage(getDriver());
-					wait.WaitTime2();
-					documentpage.clickOnNEXTButtonweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-
-				}
-				if (str.equalsIgnoreCase("Age Proof")) {
-					if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO")
-							|| testData.get("PanNameEdit").equalsIgnoreCase("YES")) {
-						if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")
-								|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
-								|| testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
-							try {
-								documentpage.SelectTypeOfDocumentAgePrrofweb(testData.get("SelectTypeOfDocAgeProof"));
-								documentpage.UploadFileweb();
-								dashboard.checkLoaderWeb();
-							} catch (Exception e) {
-
-							}
-
-						}
-					}
-
-					TestUtil.scrollTillEndOfPage(getDriver());
-					wait.WaitTime2();
-					wait.WaitTime2();
-					documentpage.clickOnSumbitweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-
-				}
-				if (str.equalsIgnoreCase("E-NACH Registration")) {
-					wait.WaitTime2();
-					TestUtil.scrollTillEndOfPage(getDriver());
-					wait.WaitTime2();
-					wait.WaitTime2();
-					documentpage.clickOnNEXTButtonweb();
-					dashboard.checkLoaderWeb();
-					wait.WaitTime2();
-
-				}
-				if (str.equalsIgnoreCase("PAN Card Proof")) {
-					if (testData.get("PanNameEdit").equalsIgnoreCase("YES")
-							|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")) {
-						documentpage.SelectTypeOfDocumentweb("PAN Card");
+						documentpage.SelectTypeOfDocumentweb(testData.get("SelectTypeOfDocument"));
 						documentpage.UploadFileweb();
 						dashboard.checkLoaderWeb();
 						wait.WaitTime2();
+						TestUtil.scrollTillEndOfPage(getDriver());
+						Thread.sleep(2000);
 						documentpage.clickOnNEXTButtonweb();
 						dashboard.checkLoaderWeb();
 						wait.WaitTime2();
@@ -293,24 +132,217 @@ public class DocumentsModule {
 						dashboard.checkLoaderWeb();
 						wait.WaitTime2();
 					}
+				} else {
+					TestUtil.scrollTillEndOfPage(getDriver());
+					wait.WaitTime2();
+					documentpage.clickOnNEXTButtonweb();
+					dashboard.checkLoaderWeb();
+					wait.WaitTime2();
 
 				}
-				if (str.equalsIgnoreCase("ECS Mandate")) {
-					documentpage.clickOnECSMAndateTileweb();
+			}
+			if (str.equalsIgnoreCase("Address Proof")) {
+				if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO")
+						|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
+						|| testData.get("EditAddress").equalsIgnoreCase("YES")) {
+					if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")
+							|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
+							|| testData.get("EditAddress").equalsIgnoreCase("YES")
+							|| testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
+						documentpage.clickOnAddressProofTileweb();
+						wait.WaitTime2();
+						documentpage.SelectTypeOfDocumentweb(testData.get("SelectTypeOfDocument"));
+						documentpage.UploadFileweb();
+						dashboard.checkLoaderWeb();
+						wait.WaitTime2();
+
+						if (testData.get("IsPanAvailable").equalsIgnoreCase("YES")
+								&& testData.get("IsNRI").equalsIgnoreCase("NO")
+								&& testData.get("EditAddress").equalsIgnoreCase("YES")
+								|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")
+								&& testData.get("IsNRI").equalsIgnoreCase("NO")
+								&& testData.get("IsPermanentAddressSame").equalsIgnoreCase("NO")
+								|| testData.get("IsPanAvailable").equalsIgnoreCase("YES")
+								&& testData.get("IsNRI").equalsIgnoreCase("YES")
+								&& testData.get("EditAddress").equalsIgnoreCase("YES")
+								|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")
+								&& testData.get("IsNRI").equalsIgnoreCase("YES")
+								&& testData.get("IsPermanentAddressSame").equalsIgnoreCase("NO")) {
+							documentpage.SelectTypeOfDocumentPermant(testData.get("SelectTypeOfDocument"));
+							documentpage.UploadFilepermant();
+							dashboard.checkLoaderWeb();
+						}
+						documentpage.clickOnNEXTButtonweb();
+						dashboard.checkLoaderWeb();
+						wait.WaitTime2();
+					} else {
+						TestUtil.scrollTillEndOfPage(getDriver());
+						wait.WaitTime2();
+						documentpage.clickOnNEXTButtonweb();
+						dashboard.checkLoaderWeb();
+						wait.WaitTime2();
+					}
+				} else {
+					TestUtil.scrollTillEndOfPage(getDriver());
 					wait.WaitTime2();
+					documentpage.clickOnNEXTButtonweb();
+					dashboard.checkLoaderWeb();
+					wait.WaitTime2();
+
+				}
+
+			}
+			if (str.equalsIgnoreCase("Non Medical Requirement")) {
+				try {
+					documentpage.UploadNonMedicalNEFTCancelChequeProposerNotInsuredweb();
+					dashboard.checkLoaderWeb();
+				} catch (Exception e) {
+
+				}
+				if (testData.get("IsNRI").equalsIgnoreCase("NO")) {
+
+					if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")) {
+						documentpage.UploadNonMedicalForm60ProposerNotInsuredweb();
+
+						dashboard.checkLoaderWeb();
+						wait.WaitTime2();
+					}
+				}
+				try {
+					documentpage.UploadNonMedicalPhotograph();
+					dashboard.checkLoaderWeb();
+				} catch (Exception e) {
+
+				}
+
+				try {
+					documentpage.UploadNonMedicalPEPQuestionnaireweb();
+					dashboard.checkLoaderWeb();
+				} catch (Exception e) {
+
+				}
+				try {
+					documentpage.BANKSTATEMENTweb();
+					dashboard.checkLoaderWeb();
+				} catch (Exception e) {
+
+				}
+				try {
+					documentpage.FINANCIALTRANSACTIONFORMweb();
+					dashboard.checkLoaderWeb();
+				} catch (Exception e) {
+
+				}
+				try {
+//					if (testData.get("IsECDF").equalsIgnoreCase("NO")) {
+					documentpage.UploadNonMedicalCustomerDeclarationFormProposerNotInsuredweb();
+					dashboard.checkLoaderWeb();
+					Thread.sleep(2000);
+
+				}catch(Exception e) {
+
+				}
+
+				TestUtil.scrollTillEndOfPage(getDriver());
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+				documentpage.clickOnNEXTButtonweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+
+			}
+			if (str.equalsIgnoreCase("Medical Requirement")) {
+				wait.WaitTime2();
+				TestUtil.scrollTillEndOfPage(getDriver());
+				wait.WaitTime2();
+				documentpage.clickOnNEXTButtonweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+			}
+			if (str.equalsIgnoreCase("Income Proof")) {
+				documentpage.clickOnIdentityTileweb();
+				wait.WaitTime2();
+				documentpage.SelectTypeOfDocumentIncomePrrofweb(testData.get("SelectTypeOfDocIncomeProof"));
+				documentpage.UploadFileweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+				TestUtil.scrollTillEndOfPage(getDriver());
+				wait.WaitTime2();
+				documentpage.clickOnNEXTButtonweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+
+			}
+			if (str.equalsIgnoreCase("Age Proof")) {
+				if (testData.get("IsAadhaarCard").equalsIgnoreCase("NO")
+						|| testData.get("PanNameEdit").equalsIgnoreCase("YES")) {
+					if (testData.get("IsPanAvailable").equalsIgnoreCase("NO")
+							|| testData.get("PanNameEdit").equalsIgnoreCase("YES")
+							|| testData.get("PanDOBEdit").equalsIgnoreCase("YES")) {
+						try {
+							documentpage.SelectTypeOfDocumentAgePrrofweb(testData.get("SelectTypeOfDocAgeProof"));
+							documentpage.UploadFileweb();
+							dashboard.checkLoaderWeb();
+						} catch (Exception e) {
+
+						}
+
+					}
+				}
+
+				TestUtil.scrollTillEndOfPage(getDriver());
+				wait.WaitTime2();
+				wait.WaitTime2();
+				documentpage.clickOnSumbitweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+
+			}
+			if (str.equalsIgnoreCase("E-NACH Registration")) {
+				wait.WaitTime2();
+				TestUtil.scrollTillEndOfPage(getDriver());
+				wait.WaitTime2();
+				wait.WaitTime2();
+				documentpage.clickOnNEXTButtonweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+
+			}
+			if (str.equalsIgnoreCase("PAN Card Proof")) {
+				if (testData.get("PanNameEdit").equalsIgnoreCase("YES")
+						|| testData.get("IsPanAvailable").equalsIgnoreCase("NO")) {
+					documentpage.SelectTypeOfDocumentweb("PAN Card");
 					documentpage.UploadFileweb();
 					dashboard.checkLoaderWeb();
-					TestUtil.scrollTillEndOfPage(driver);
-					Thread.sleep(9000);
+					wait.WaitTime2();
+					documentpage.clickOnNEXTButtonweb();
+					dashboard.checkLoaderWeb();
+					wait.WaitTime2();
+				} else {
+					TestUtil.scrollTillEndOfPage(getDriver());
+					wait.WaitTime2();
 					documentpage.clickOnNEXTButtonweb();
 					dashboard.checkLoaderWeb();
 					wait.WaitTime2();
 				}
 
 			}
-			wait.WaitTime2();
-			documentpage.ClickOnCSEAlertOKButton();
-			dashboard.checkLoaderWeb();
+			if (str.equalsIgnoreCase("ECS Mandate")) {
+				documentpage.clickOnECSMAndateTileweb();
+				wait.WaitTime2();
+				documentpage.UploadFileweb();
+				dashboard.checkLoaderWeb();
+				TestUtil.scrollTillEndOfPage(driver);
+				Thread.sleep(9000);
+				documentpage.clickOnNEXTButtonweb();
+				dashboard.checkLoaderWeb();
+				wait.WaitTime2();
+			}
+
 		}
+		wait.WaitTime2();
+		documentpage.ClickOnCSEAlertOKButton();
+		dashboard.checkLoaderWeb();
+	}
 
 }
